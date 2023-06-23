611f58e9a9511fe5b13846d70fb5e835ef3142c8
AdamFlair1M <njabuloflair@gmail.com>
2023-06-22 17:08:51 -0700

    majorFix

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * largest_prime_factor - Finds the largest prime factor of a number
+ * @n: The number
+ *
+ * Return: The largest prime factor of the number
+ */
+unsigned long largest_prime_factor(unsigned long n)
+{
+	unsigned long largest_factor = 2;
+
+	while (n > largest_factor)
+	{
+		if (n % largest_factor == 0)
+		{
+			n /= largest_factor;
+		}
+		else
+		{
+			largest_factor++;
+		}
+	}
+
+	return (largest_factor);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long number = 612852475143;
+	unsigned long largest_prime;
+
+	largest_prime = largest_prime_factor(number);
+
+	printf("%lu\n", largest_prime);
+
+	return (0);
+}
